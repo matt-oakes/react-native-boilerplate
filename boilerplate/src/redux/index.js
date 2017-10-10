5afe983ef7f9bbad1c3d5f98712605c2f04b549c
Matt Oakes <hello@mattoakes.net>
2017-10-10 11:13:13 +0100

    Move to react-persist v5
    Fixes #3

--- boilerplate/src/redux/index.js
+++ boilerplate/src/redux/index.js
@@ -1,45 +1,39 @@
 // @flow
 
-import { AsyncStorage } from "react-native";
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "remote-redux-devtools";
 import createSagaMiddleware from "redux-saga";
-import { persistStore, autoRehydrate } from "redux-persist";
-import { createBlacklistFilter } from "redux-persist-transform-filter";
-import createMigration from "redux-persist-migrate";
+import { persistStore, persistReducer, createMigration } from 'redux-persist'
+import PersistStorage from 'redux-persist/es/storage' // default: localStorage if web, AsyncStorage if react-native
 
-import migrationManifest from "./migrations";
-import appReducer from "./reducers";
+import migrations from "./migrations";
+import rootReducer from "./reducers";
 import RootSaga from "../sagas";
 import type { StoreType } from "./types";
 
 export { initialState } from "./reducers";
 
-const sagaMiddleware = createSagaMiddleware();
-
-const migration = createMigration(migrationManifest, "persist");
+const persistConfig = {
+  key: "persistKey",
+  storage: PersistStorage,
+  version: 1,
+  migrate: createMigration(migrations, { debug: false }),
+  whitelist: []
+};
+const appReducer = persistReducer(persistConfig, rootReducer)
 
 const composeEnhansers = composeWithDevTools({
   hostname: "localhost",
   port: 8000
 });
-const store: StoreType = createStore(
+const store: StoreType = persistStore(
   appReducer,
-  composeEnhansers(migration, autoRehydrate(), applyMiddleware(sagaMiddleware))
+  {
+    enhancer: () => composeEnhansers(applyMiddleware(createSagaMiddleware()))
+  }
 );
 
 // Start the root saga running which starts all the others
 sagaMiddleware.run(RootSaga);
-
-// Begin periodically persisting the store with the whitelisted reducer names
-persistStore(store, {
-  whitelist: ["persist", "outlet", "user"],
-  transforms: [
-    // We don't want to store all the outlets through persist, as that's a lot of data to load on startup
-    // TODO: Store the outlets in another way so they can work offline
-    createBlacklistFilter("outlet", ["outlets"])
-  ],
-  storage: AsyncStorage
-});
 
 export default store;
