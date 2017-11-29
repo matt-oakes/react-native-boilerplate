2fe62165e9fd8da8dca5f2d86defabde8b70946a
Matt Oakes <hello@mattoakes.net>
2017-11-29 11:10:19 +0000

    Upgrade to React Native 0.50.4. Closes #8

---
+++ boilerplate/rn-cli.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getEnableBabelRCLookup() {
+    return true;
+  }
+};
--- boilerplate/src/models/apiError.js
+++ boilerplate/src/models/apiError.js
@@ -2,26 +2,6 @@
 
 import I18n from "react-native-i18n";
 
-import * as c from "~/src/lib/converter";
-import type { Converter } from "~/src/lib/converter";
-
-function convertResponseError(error): APIError {
-  return new APIError(
-    error.code || -1,
-    error.title || "Unknown error",
-    error.detail || {}
-  );
-}
-
-export const errorConverter: Converter<APIError> = c.compose(
-  convertResponseError,
-  c.object({
-    code: c.field("error_code", c.maybe(c.integer)),
-    title: c.field("error_title", c.maybe(c.string)),
-    detail: c.field("error_detail", c.objectValues(c.string))
-  })
-);
-
 export type APIErrorDetailType = {
   [field: string]: string
 };
@@ -36,7 +16,8 @@ export function APIError(
   code: number = -1,
   title: ?string = null,
   detail: APIErrorDetailType = {}
-) {
+// $FlowExpectedError Needed to force this to be of type "Error"
+): Error {
   this.code = code;
   this.title = title || I18n.t("error.unknown");
   this.detail = detail;
