4f72913ce09fa30b8d0de85932181e78113c8945
Matt Oakes <hello@mattoakes.net>
2017-12-08 17:00:05 +0000

    Add a saga generator

--- commands/saga.js
+++ commands/saga.js
@@ -1,28 +1,26 @@
-// @cliDescription  Generates a screen.
+// @cliDescription  Generates a saga.
 
 module.exports = async function (context) {
   // grab some features
-  const { parameters, strings, print, ignite, system } = context
-  const { camelCase, pascalCase, isBlank } = strings
+  const { parameters, strings, print, ignite } = context
+  const { camelCase, isBlank } = strings
 
   // validation
   if (isBlank(parameters.first)) {
-    print.info(`${context.runtime.brand} generate screen <name>\n`)
+    print.info(`${context.runtime.brand} generate saga <name>\n`)
     print.info('A name is required.')
     return
   }
 
   let pathComponents = parameters.first.split('/').map(camelCase)
-  let name = pascalCase(pathComponents.pop())
+  let name = pathComponents.pop()
   const relativePath = pathComponents.length ? pathComponents.join('/') + '/' : ''
 
   const props = { name }
   const jobs = [{
-    template: 'screen.ejs',
-    target: `src/screens/${relativePath}${name}.js`
+    template: 'saga.ejs',
+    target: `src/sagas/${relativePath}${name}.js`
   }]
 
   await ignite.copyBatch(context, jobs, props)
-
-  await system.run(`yarn update-storybook`)
 }
