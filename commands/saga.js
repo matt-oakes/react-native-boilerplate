3c216675c13223fd7bb03c9abfb338e2f51714d7
Matt Oakes <hello@mattoakes.net>
2017-12-08 17:52:27 +0000

    Automatically import the new saga

--- commands/saga.js
+++ commands/saga.js
@@ -2,7 +2,7 @@
 
 module.exports = async function (context) {
   // grab some features
-  const { parameters, strings, print, ignite } = context
+  const { parameters, strings, print, ignite, filesystem } = context
   const { camelCase, isBlank } = strings
 
   // validation
@@ -23,4 +23,18 @@ module.exports = async function (context) {
   }]
 
   await ignite.copyBatch(context, jobs, props)
+
+  // Update the index file
+  const filename = 'src/sagas/index.js'
+  const contents = await filesystem.readAsync(filename, 'utf8');
+  const mutatedContents = contents
+    .replace(
+      /\[((?:\s*?call\(.*\),?)*)\s*\]/gm,
+      `[$1, call(${name}Saga)]`
+    )
+    .replace(
+      /(\s*)((?:\s*import .*Saga from "\.\/.*";)+)(\s*)/gm,
+      `$1$2\nimport ${name}Saga from "./${name}";$3`
+    )
+  await filesystem.writeAsync(filename, mutatedContents, { atomic: true })
 }
